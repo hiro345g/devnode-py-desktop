869c74b55402ebd8c161e8898dbb5998d48584a8
hiro345g <hiro345g@gmail.com>
2023-04-09 08:39:11 +0900

    sed による置換処理で文字列 devnode-py-desktop を対象とするように修正

--- workspace_share/script/create-devnode-py-desktop-mozc.sh
+++ workspace_share/script/create-devnode-py-desktop-mozc.sh
@@ -14,7 +14,7 @@ if [ ! -e ${DIST_DIR}/.devcontainer ]; then mkdir -p ${DIST_DIR}/.devcontainer; 
 if [ ! -e ${DIST_DIR}/workspace_share ]; then mkdir -p ${DIST_DIR}/workspace_share; fi
 cp -r ${DEV_DIR}/devnode-py-desktop/.devcontainer/* ${DIST_DIR}/.devcontainer/
 cp -r ${DEV_DIR}/devnode-py-desktop/docker-compose.yml ${DIST_DIR}/
-sed -i 's/devnode-desktop/devnode-py-desktop-mozc/' ${DIST_DIR}/.devcontainer/devcontainer.json
-sed -i 's/devnode-desktop/devnode-py-desktop-mozc/' ${DIST_DIR}/docker-compose.yml
+sed -i 's/devnode-py-desktop/devnode-py-desktop-mozc/' ${DIST_DIR}/.devcontainer/devcontainer.json
+sed -i 's/devnode-py-desktop/devnode-py-desktop-mozc/' ${DIST_DIR}/docker-compose.yml
 
 rm -fr ${DEV_DIR}/devnode-py-desktop
